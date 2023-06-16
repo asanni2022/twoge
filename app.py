9dd18f3c97b304a338d8b5361ed6bf547d4adeec
akim training <akeem.sql01@gmail.com>
2023-06-16 17:39:11 -0500

    yml update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
